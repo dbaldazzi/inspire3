87697136e218f8453f672e2ae21e445713cb50ec
dbaldazzi <d.baldazzi@hotmail.com>
2019-08-30 12:07:42 -0600

    updated models

--- app/models/image.js
+++ app/models/image.js
@@ -1,5 +1,7 @@
 export default class image {
   constructor(data) {
-
+    this.id = data.id
+    this.url = data.url
+    this.large_url = data.large_url
   }
 }
--- app/models/quote.js
+++ app/models/quote.js
@@ -1,5 +1,9 @@
 export default class quote {
   constructor(data) {
+    this.id = data.id
+    this.url = data.url
+    this.author = data.author
+    this.body = data.body
 
   }
 }
