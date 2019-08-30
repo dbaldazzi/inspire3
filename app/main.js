0b3c7dbdc9af6808c4a225df82982be25b2f3fe8
dbaldazzi <d.baldazzi@hotmail.com>
2019-08-30 14:38:45 -0600

    main js linked to all controllers

--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
 import WeatherController from "./controllers/weather-controller.js";
 import TodoController from "./controllers/todo-controller.js";
+import ImageController from "./controllers/image-controller.js";
+import QuoteController from "./controllers/quote-controller.js";
+
 
 //TODO Dont forget to register all your controllers
 class App {
